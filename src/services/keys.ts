70ac6e9061ee1184e62f80fb385decd3132a8763
HongSeongJo <164273757+HongSeongJo@users.noreply.github.com>
2026-02-18 16:56:57 +0900

    feat: 아이템 생성 로직

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,3 +1,4 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionsKey = (sessinId: string) => `sessions#${sessinId}`;
+export const itemsKey = (itemId: string) => `items#${itemId}`;
