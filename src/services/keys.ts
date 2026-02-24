476422da718af3f47df4fb02615c31c5badb9f4f
HongSeongJo <164273757+HongSeongJo@users.noreply.github.com>
2026-02-24 16:26:55 +0900

    feat: 아이템에 대한 Sorted Set 초기화

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,7 +1,10 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionsKey = (sessinId: string) => `sessions#${sessinId}`;
-export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const usernamesUniqueKey = () => 'usernames:unique';
 export const userLikesKey = (userId: string) => `users:likes#${userId}`;
 export const usernameKey =  () => 'usernames';
+
+// Items
+export const itemsKey = (itemId: string) => `items#${itemId}`;
+export const itemsByViewsKey = () => 'items:views';
