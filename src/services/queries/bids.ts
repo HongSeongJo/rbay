b70ad7aa010168f1d46df101ccc634eb19ee6c66
HongSeongJo <164273757+HongSeongJo@users.noreply.github.com>
2026-02-27 13:15:15 +0900

    fix: 입찰 정보 업데이트

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,6 +1,6 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
 import { client } from '$services/redis';
-import { bidHistoryKey } from '$services/keys';
+import { bidHistoryKey, itemsKey } from '$services/keys';
 import { DateTime } from 'luxon';
 import { getItem } from './items';
 
@@ -24,7 +24,14 @@ export const createBid = async (attrs: CreateBidAttrs) => {
 		attrs.createdAt.toMillis()
 	);
 
-	return client.rPush(bidHistoryKey(attrs.itemId), serialized);
+	return Promise.all([
+		client.rPush(bidHistoryKey(attrs.itemId), serialized),
+		client.hSet(itemsKey(item.id), {
+			bids: item.bids + 1,
+			price: attrs.amount,
+			highestBidUserId: attrs.userId
+		})
+	]);
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
